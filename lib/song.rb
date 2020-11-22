c62adcb562488e1775259e50f55aad990b504935
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-22 02:04:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,24 +2,14 @@ class Song
   @@count = 0
   @@genre  = []
 
+  attr_reader :name, :artist, :genre
+
   def initialize(name, artists, genres)
     @name = name
     @artist = artists
     @genre = genres
     @@count += 1
-
-
   end
 
-  def name
-    @name
-  end
 
-  def artist
-    @artist
-  end
-
-  def genre
-    @genre
-  end
 end
