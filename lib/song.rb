902ef5547c7098efc52cae80bc37810e16de63ac
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-22 02:14:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
 class Song
   @@count = 0
+  
   @@genres  = []
   @@artists = []
+
+  @@genre_count = {"rap" => 0, "rock" => 0, "pop" => 0}
+  @@artist_count = {"Jay-Z" => 0, "Drake" => 0, "Beyonce" => 0}
 
   attr_reader :name, :artist, :genre
 
@@ -12,6 +16,8 @@ class Song
     @@count += 1
     @@genres << @genre
     @@artists << @artist
+    @@genre_count[@genre] += 1
+    @@artist_count[@artist] += 1
   end
 
   def self.count
@@ -22,8 +28,16 @@ class Song
     @@artists.uniq
   end
 
+  def self.artist_count
+    @@artist_count
+  end
+
   def self.genres
     @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genre_count
   end
 
 end
