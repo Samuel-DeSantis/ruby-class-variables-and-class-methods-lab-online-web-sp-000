c10ff686cd38395bfc675f27a63577a74408f2a0
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-22 04:32:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,19 +27,19 @@ class Song
   end
 
   def create_genre_count
-    @@genres.uniq.each { |genre| @@genre_count[genre] = 1 }
+    @@genres.uniq.each { |genre| @@genre_count[genre] = 0 }
   end
 
   def fill_genre_count
-    @@genres.uniq.each { |genre| @@genre_count[genre] += 1}
+    @@genres.each { |genre| @@genre_count[genre] += 1}
   end
 
   def create_artist_count
-    @@artists.uniq.each { |artist| @@artist_count[artist] = 1 }
+    @@artists.uniq.each { |artist| @@artist_count[artist] = 0 }
   end
 
   def fill_artist_count
-    @@artists.uniq.each { |artist| @@artist_count[artist] += 1}
+    @@artists.each { |artist| @@artist_count[artist] += 1}
   end
 
   def self.count
